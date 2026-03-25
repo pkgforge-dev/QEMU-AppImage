44f6631ef53900b44b314df8ec9690684e434164
Link4Electronics <link4electronics@gmail.com>
2026-03-25 11:38:30 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -17,7 +17,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/qemu-system-x86_64 /usr/bin/qemu-system-i386 /usr/lib/qemu/ui-gtk.so
+quick-sharun /usr/bin/qemu-system-x86_64 /usr/bin/qemu-system-i386 /usr/lib/qemu/ui-gtk.so /usr/lib/qemu/audio-pa.so
 
 # Additional changes can be done in between here
 
