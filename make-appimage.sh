c3180efc9d3cc7bf8892d3afd785e02ea4a78653
Link4Electronics <link4electronics@gmail.com>
2026-04-13 06:16:33 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,14 +3,12 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q qemu-full | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q qemu-full | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://gitlab.com/qemu-project/qemu/-/raw/master/ui/icons/qemu.svg
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
@@ -20,17 +18,12 @@ export OPTIMIZE_LAUNCH=1
 quick-sharun \
 	/usr/bin/qemu-*      \
 	/usr/lib/qemu/*.so   \
-	/usr/bin/bash        \
 	/usr/bin/zenity      \
 	/usr/bin/spicy       \
-	/usr/bin/quickemu    \
-	/usr/bin/quickget    \
-	/usr/bin/quickreport \
 	/usr/share/edk2      \
+	/usr/share/qemu		 \
 	/usr/lib/binfmt.d	 \
-	/usr/lib/libcapstone.so.5
-
-# Additional changes can be done in between here
+	/usr/lib/libcapstone.so*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
